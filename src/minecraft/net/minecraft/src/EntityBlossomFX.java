991e2846acc336475c366115378bdcb68086e129
disckord <56471145+disckord@users.noreply.github.com>
2019-12-08 11:13:02 +1100

    work towards 1.1.7
    worked on cherry blossoms

--- src/minecraft/net/minecraft/src/EntityBlossomFX.java
+++ src/minecraft/net/minecraft/src/EntityBlossomFX.java
@@ -3,7 +3,8 @@ package net.minecraft.src;
 public class EntityBlossomFX extends EntityFX
 {
     float particleScaleOverTime;
-
+    int i;
+    boolean stop = false;
     public EntityBlossomFX(World par1World, double par2, double par4, double par6, double par8, double par10, double par12)
     {
         this(par1World, par2, par4, par6, par8, par10, par12, 2.0F);
@@ -11,34 +12,22 @@ public class EntityBlossomFX extends EntityFX
 
     public EntityBlossomFX(World par1World, double par2, double par4, double par6, double par8, double par10, double par12, float par14)
     {
-        super(par1World, par2, par4, par6, 0.0D, 0.0D, 0.0D);
+        super(par1World, par2, par4+1.5D, par6, 0.0D, 0.0D, 0.0D);
         //this.motionX *= 0.009999999776482582D;
        // this.motionY *= 0.009999999776482582D;
       //  this.motionZ *= 0.009999999776482582D;
-       // this.motionY += 0.1D;
+        this.motionY = -0.05D - (rand.nextFloat() * 0.04f);
         this.particleScale *= 0.75F;
         this.particleScale *= par14;
         this.particleScaleOverTime = this.particleScale;
-        this.particleMaxAge = 16;
+        this.particleMaxAge = 80;
         this.noClip = false;
-        this.setParticleTextureIndex(80);
+        this.setParticleTextureIndex(15);
     }
 
     public void renderParticle(Tessellator par1Tessellator, float par2, float par3, float par4, float par5, float par6, float par7)
     {
-        float var8 = ((float)this.particleAge + par2) / (float)this.particleMaxAge * 32.0F;
-
-        if (var8 < 0.0F)
-        {
-            var8 = 0.0F;
-        }
-
-        if (var8 > 1.0F)
-        {
-            var8 = 1.0F;
-        }
-
-        this.particleScale = this.particleScaleOverTime * var8;
+     
         super.renderParticle(par1Tessellator, par2, par3, par4, par5, par6, par7);
     }
 
@@ -47,6 +36,19 @@ public class EntityBlossomFX extends EntityFX
      */
     public void onUpdate()
     {
+    	if(onGround)
+    	{
+    		stop=true;
+    	}
+    	if(!stop)
+    	{
+    	i = 15 - (this.particleAge + 20) * 8 / (this.particleMaxAge);
+    	if(i<=8)
+    	{
+    		i=8;
+    	}
+    	this.setParticleTextureIndex(i);
+    	}
         this.prevPosX = this.posX;
         this.prevPosY = this.posY;
         this.prevPosZ = this.posZ;
@@ -58,20 +60,21 @@ public class EntityBlossomFX extends EntityFX
 
         this.moveEntity(this.motionX, this.motionY, this.motionZ);
 
-        if (this.posY == this.prevPosY)
+       /* if (this.posY == this.prevPosY)
         {
             this.motionX *= 1.1D;
             this.motionZ *= 1.1D;
-        }
+        }*/
 
-        this.motionX *= 0.8600000143051147D;
-        this.motionY *= 0.8600000143051147D;
-        this.motionZ *= 0.8600000143051147D;
+        this.motionX *= 0.9600000143051147D;
+       // this.motionY *= 0.8600000143051147D;
+        this.motionZ *= 0.9600000143051147D;
 
         if (this.onGround)
         {
-            this.motionX *= 0.699999988079071D;
-            this.motionZ *= 0.699999988079071D;
+        	
+            this.motionX *= 0.099999988079071D;
+            this.motionZ *= 0.099999988079071D;
         }
     }
 }
